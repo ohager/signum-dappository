0ce1e6c494c4803c27ad53cb59754e39a896be1a
ohager <oliver.hager@dextra-sw.com>
2019-12-21 20:00:51 -0300

    feat: Donation Input fine

--- src/features/donation/Donation.svelte
+++ src/features/donation/Donation.svelte
@@ -34,43 +34,66 @@
         margin: 0.5rem
     }
 
+    .donation__header {
+        text-align: center;
+        margin-bottom: 1rem;
+    }
+
+    .donation__header img {
+        height: 96px;
+        width: 96px;
+        text-align: center;
+    }
+
     .donation__form {
         display: block;
         max-width: 600px;
         margin: 0 auto
     }
 
-    .donation__header {
+    .donation__form--header {
         display: flex;
         flex-direction: row;
         align-items: center;
+        margin-bottom: 1rem;
     }
 
-    .donation__header img {
+    .donation__form--header img {
         height: 64px;
         width: 64px;
         padding-right: 1rem;
+    }
+
+    .donation__form--input {
+        display: flex;
+        align-items: flex-end;
+    }
+
+    :global(.mdc-text-field__input) {
+        font-size: 2rem !important;
+    }
+
+    :global(.mdc-text-field) {
+        width: 100% !important;
     }
 
 </style>
 
 <div class="donation">
     <Paper>
-        <div class="header">
+        <div class="donation__header">
             <img src="/img/donation.svg" alt="donate">
-            <h4 class="mdc-typography--headline4">
-                Donate to
-            </h4>
         </div>
         <div class="donation__form">
-            <div class="donation__header">
+            <div class="donation__form--header">
                 <img src={token.img} alt={`${token.name}-logo`}>
                 <h4 class="mdc-typography--headline4">
                     {token.name}
                 </h4>
             </div>
-            <div class="donation__input">
-                <TextField fullwidth bind:value={amount} label="Donation Amount"/>
+            <div class="donation__form--input">
+                <TextField bind:value={amount} label="Donation Amount"/>
+                <span class="mdc-typography--headline6">BURST</span>
             </div>
         </div>
     </Paper>
