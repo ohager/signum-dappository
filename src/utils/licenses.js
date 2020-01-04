5a0c9ac251b902310da8c37b99bd6f51ca3d5ab4
ohager <oliver.hager@dextra-sw.com>
2020-01-04 10:14:29 -0300

    licenses constants

--- src/utils/licenses.js
+++ src/utils/licenses.js
@@ -1,19 +1,6 @@
-export const LicenseMIT = {
-    slug:'MIT',
-    description: ''
-}
-
-export const LicenseISC = {
-    slug:'ISC',
-    description: ''
-}
-
-export const LicenseApache2 = {
-    slug:'Apache-2.0',
-    description: ''
-}
-
-export const LicenseGPL3 = {
-    slug:'GPL-3.0',
-    description: ''
+export const Licenses = {
+    MIT: '',
+    ISC: '',
+    'Apache-2.0' : '',
+    'GPL-3.0': ''
 }
