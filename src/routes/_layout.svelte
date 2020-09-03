53ada4bc38bca17a24fe822f8126289f4f5635c5
ohager <oliver.hager@dextra-sw.com>
2020-09-03 12:43:50 -0300

    chore: minor cleanup

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -9,11 +9,6 @@
 <script>
     import { Header, Message, AccountDialog, LoadingBar, ThemeProvider } from '../features/_common'
     import LeftSideMenu from '../features/_common/LeftSideMenu.svelte'
-    import { Scrim } from '@smui/drawer'
-    import Stamp from '../features/_common/Stamp.svelte'
-
-    export let segment
-    // export let preload
     export let isTestnet
 
     let isMenuOpen = false
