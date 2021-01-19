d450aca6b44d70f365297cc49519f35b1cd72274
ohager <oliver.hager@dextra-sw.com>
2021-01-19 11:08:24 -0300

    chore: updated now to vercel

--- src/context/explorer.js
+++ src/context/explorer.js
@@ -5,7 +5,7 @@ const explorer = path => `${Vars.ExplorerUrl}${path}`
 export const ExplorerApi = {
     getBlockUrl: blockId => {
         console.info('Explorer does not support block urls')
-        //explorer(`block/${blockId}`)
+        // explorer(`block/${blockId}`)
     },
     getAccountUrl: accountId => explorer(`?action=account&account=${accountId}`),
 }
