c81cfca2ad3ce8b29f43333d370fb9cf2a295a4e
ohager <oliver.hager@dextra-sw.com>
2021-01-19 11:12:13 -0300

    chore: removed comment - vercel listen to git?

--- src/context/explorer.js
+++ src/context/explorer.js
@@ -5,7 +5,6 @@ const explorer = path => `${Vars.ExplorerUrl}${path}`
 export const ExplorerApi = {
     getBlockUrl: blockId => {
         console.info('Explorer does not support block urls')
-        // explorer(`block/${blockId}`)
     },
     getAccountUrl: accountId => explorer(`?action=account&account=${accountId}`),
 }
