ceb36d65d2ffa9251857ec3d0db47b847185c9a8
ohager <oliver.hager@dextra-sw.com>
2021-01-19 11:20:48 -0300

    feat: block height explorer link

--- src/context/explorer.js
+++ src/context/explorer.js
@@ -3,8 +3,6 @@ import { Vars } from './vars'
 const explorer = path => `${Vars.ExplorerUrl}${path}`
 
 export const ExplorerApi = {
-    getBlockUrl: blockId => {
-        explorer(`action=blocks&height=/${blockId}`)
-    },
+    getBlockUrl: blockId => explorer(`?action=blocks&height=${blockId}`),
     getAccountUrl: accountId => explorer(`?action=account&account=${accountId}`),
 }
