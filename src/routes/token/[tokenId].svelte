149c5a653f6642d7b24dde31896888be1d90ba1f
ohager <oliver.hager@dextra-sw.com>
2021-05-09 19:37:24 -0300

    feat: ssr meta tags for tokens

--- src/routes/token/[tokenId].svelte
+++ src/routes/token/[tokenId].svelte
@@ -1,17 +1,45 @@
 <script context="module">
+    import { BurstApi } from '../../context'
+    import { ApplicationToken } from '../../services/repositories/models/applicationToken'
+
     export async function preload({ params, query }) {
         const {tokenId} = params;
-        return {tokenId}
+
+        const contract = await BurstApi.contract.getContract(tokenId)
+        const tokenData = ApplicationToken.mapFromContract(contract)
+        return {tokenId, tokenData}
     }
 </script>
+
 
 <script>
     import { tokens$ } from '../../features/_common/tokenStore'
     import TokenDetail from '../../features/tokens/TokenDetail.svelte'
 
     export let tokenId
+    export let tokenData
     $: token = $tokens$.items.find(i => i.at === tokenId)
 </script>
+
+<svelte:head>
+    <!-- Google / Search Engine Tags -->
+    <meta itemprop="name" content={`The Burst dAppository - ${tokenData.name}`}>
+    <meta itemprop="description" content={tokenData.desc}>
+    <meta itemprop="image" content={tokenData.img}>
+
+    <!-- Facebook Meta Tags -->
+    <meta property="og:type" content="website">
+    <meta itemprop="og:title" content={`The Burst dAppository - ${tokenData.name}`}>
+    <meta property="og:description" content={tokenData.desc}>
+    <meta property="og:image" content={tokenData.img}>
+
+    <!-- Twitter Meta Tags -->
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:title" content={`The Burst dAppository - ${tokenData.name}`}>
+    <meta name="twitter:description" content={tokenData.desc}>
+    <meta name="twitter:image" content={tokenData.img}>
+</svelte:head>
+
 
 {#if token}
     <TokenDetail {token} />
