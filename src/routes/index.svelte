2e7eb7b5c7b7e917a4743e73b1b11b95560b7072
ohager <oliver.hager@dextra-sw.com>
2021-05-09 20:43:11 -0300

    fix: moved generic meta tags to index

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -31,6 +31,25 @@
     }
 </script>
 
+<svelte:head>
+    	<!-- Google / Search Engine Tags -->
+    	<meta itemprop="name" content="The Burst dAppository">
+    	<meta itemprop="description" content="">
+    	<meta itemprop="image" content="https://raw.githubusercontent.com/ohager/burst-dappository/develop/static/img/logo.black.svg">
+
+    	<!-- Facebook Meta Tags -->
+    	<meta property="og:type" content="website">
+    	<meta property="og:title" content="The Burst dAppository">
+    	<meta property="og:description" content="The Burst dAppository is the place to find tools, apps, and more resources for the Burstcoin Ecosystem.">
+    	<meta property="og:image" content="https://raw.githubusercontent.com/ohager/burst-dappository/develop/static/img/logo.black.png">
+
+    	<!-- Twitter Meta Tags -->
+    	<meta name="twitter:card" content="summary_large_image">
+    	<meta name="twitter:title" content="The Burst dAppository">
+    	<meta name="twitter:description" content="The Burst dAppository is the place to find tools, apps, and more resources for the Burstcoin Ecosystem.">
+    	<meta name="twitter:image" content="https://raw.githubusercontent.com/ohager/burst-dappository/develop/static/img/logo.black.png">
+</svelte:head>
+
 <div>
     <TokenList {searchText} />
     <RegisterFabButton on:mouseenter={prefetchRoute} on:click={handleClick}/>
