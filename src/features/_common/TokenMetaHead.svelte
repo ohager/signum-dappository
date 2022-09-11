f842166e37c16cd44bf37bd656790ee2539327a1
ohager <oliver@devbutze.com>
2022-09-11 14:58:44 -0300

    fix: meta tag data for token

--- src/features/_common/TokenMetaHead.svelte
+++ src/features/_common/TokenMetaHead.svelte
@@ -4,19 +4,19 @@
 
 <svelte:head>
   <!-- Google / Search Engine Tags -->
-  <meta itemprop="name" content={`The Burst DAppository - ${tokenData.name}`}>
+  <meta itemprop="name" content={`The Signum DAppository - ${tokenData.name}`}>
   <meta itemprop="description" content={tokenData.desc}>
   <meta itemprop="image" content={tokenData.img}>
 
   <!-- Facebook Meta Tags -->
   <meta property="og:type" content="website">
-  <meta itemprop="og:title" content={`The Burst DAppository - ${tokenData.name}`}>
+  <meta itemprop="og:title" content={`The Signum DAppository - ${tokenData.name}`}>
   <meta property="og:description" content={tokenData.desc}>
   <meta property="og:image" content={tokenData.img}>
 
   <!-- Twitter Meta Tags -->
   <meta name="twitter:card" content="summary_large_image">
-  <meta name="twitter:title" content={`The Burst DAppository - ${tokenData.name}`}>
+  <meta name="twitter:title" content={`The Signum DAppository - ${tokenData.name}`}>
   <meta name="twitter:description" content={tokenData.desc}>
   <meta name="twitter:image" content={tokenData.img}>
 </svelte:head>
