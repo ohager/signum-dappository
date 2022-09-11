6864cd06e2f6b90bfeed3786a224d81dab3d0bcc
ohager <oliver@devbutze.com>
2022-09-11 15:23:58 -0300

    feat: introduced ledger service

---
+++ src/services/ledgerService.js
@@ -0,0 +1,25 @@
+import { Vars } from '../context'
+import { LedgerClientFactory } from '@signumjs/core'
+
+export class LedgerService {
+    constructor() {
+        this.ledger = LedgerClientFactory.createClient({
+            nodeHost: Vars.HostUrl,
+        })
+    }
+
+    get client() {
+        return this.ledger
+    }
+
+    updateNode(nodeHost) {
+        if (nodeHost !== this.ledger.service.settings.nodeHost) {
+            this.ledger = LedgerClientFactory.createClient({
+                nodeHost: nodeHost,
+            })
+            console.debug('Update node to', nodeHost)
+        }
+    }
+}
+
+export const ledgerService = new LedgerService()
